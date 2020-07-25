cef42d97055eee794456abec9c788a34f76c23c0
Lazar Pešić <lazar.pesic0109@gmail.com>
2020-07-25 19:48:13 +0200

    controllers renamed, app diagram

---
+++ PBlog/PBlog/Views/Controllers/PreviewViewController.swift
@@ -0,0 +1,42 @@
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class PreviewViewController: UIViewController, Storyboard {
+    
+    var eventsSubject = PublishSubject<Event>()
+    var db = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
+    }
+    
+}
+
+
+//MARK: Coordinated
+
+extension PreviewViewController: Coordinated {
+    
+    enum Event {
+        case finished
+    }
+    
+    var events: Observable<PreviewViewController.Event> {
+        return eventsSubject
+    }
+}
+
+
+//MARK: Bindings
+
+fileprivate extension PreviewViewController {
+    
+    func bind() {
+        //call methods
+    }
+
+    //definition binding methods
+}
